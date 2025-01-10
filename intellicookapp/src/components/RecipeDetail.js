a8439cb1db1f8bd2efae115524ee289d3a2539af
Tobias <bouviertobiaspro@gmail.com>
2025-01-10 14:44:23 +0100

    Build: non correction du boutton favoris RecipeDetail

--- intellicookapp/src/components/RecipeDetail.js
+++ intellicookapp/src/components/RecipeDetail.js
@@ -26,13 +26,13 @@ function RecipeDetail() {
 
       <h1>{recipe.name}</h1>
       <div className="image_block">
-      <img src={recipe.image} alt={recipe.name}></img>
-      <button onClick={handleFavoriteClick}>
-      <img src="./icones/Icone-Heart-White.svg" alt=""></img>      
-        {isFavorite(recipe.name)
-          ? "Retirer des Favoris"
-          : "Ajouter aux Favoris"}
-      </button>
+        <img src={recipe.image} alt={recipe.name}></img>
+        <button onClick={handleFavoriteClick}>
+          <img src="./icones/Icone-Heart-White.svg" alt=""></img>
+          {isFavorite(recipe.name)
+            ? "Retirer des Favoris"
+            : "Ajouter aux Favoris"}
+        </button>
       </div>
       <div className="ingredients_block">
         <h2>Ingrédients :</h2>
