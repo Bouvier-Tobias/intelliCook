5d9052ee8e08c80f69e4e64ef489ef32eef95b32
RobinDelbart05 <bamboomonkey05@gmail.com>
2025-01-13 10:31:46 +0100

    fix : resolve conflicts

--- intellicookapp/src/pages/Catalogue.js
+++ intellicookapp/src/pages/Catalogue.js
@@ -1,18 +1,72 @@
+
+
+
+
+
+
 import { useEffect, useState } from "react";
-export default function Recette() {
+
+
+// const Recette = () => {
+//   const [recette, setRecette] = useState([]);
+
+//   useEffect(() => {
+//     fetch("recette.json")
+//       .then((response) => response.json())
+//       .then((data) => setRecette(data.recette))
+//       .catch((error) => console.log(error));
+//   }, []);
+
+//   return (
+//     <div className="recette">
+//       <h1>Nom de la recette</h1>
+//       <ul>
+//         {recette.map((recette, index) => (
+//           <li key={index}>
+//             <h2>{recette.titre}</h2>
+//             <ul>
+//               {/* Boucle sur les ingrédients de chaque recette */}
+//               {recette.ingredients.map((ingredient, i) => (
+//                 <li key={i}>{ingredient.quantité} {ingredient.suite} {ingredient.nom}</li>
+//               ))}
+//             </ul>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+
+const Recette = () => {
   const [recette, setRecette] = useState([]);
+
   useEffect(() => {
     fetch("recette.json")
       .then((response) => response.json())
       .then((data) => setRecette(data.recette))
       .catch((error) => console.log(error));
   }, []);
-  function ListRecette() {
-    return (
-      <div>
-        <h1>Recette</h1>
-      </div>
-    );
-  }
-}
-return <ListRecette />;
+
+  return (
+    <div className="recette">
+      <h1>{recette[1].titre}</h1>
+
+      {/* Affichage des ingrédients de la première recette */}
+      {recette.length > 0 && (
+        <ul>
+          {recette[1].ingredients.map((ingredient, index) => (
+            <li key={index}>
+              {ingredient.quantité} {ingredient.suite} {ingredient.nom}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+
+
+export default Recette;
+
